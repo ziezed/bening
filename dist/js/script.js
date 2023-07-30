10c4171bb97006ec9aa31a5fc7c1fc9dc66070a0
ziezed <ziezed@yahoo.co.id>
2023-07-30 21:17:56 +0700

    pertama

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+// navabr fixed
+window.onscroll = function(){
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add('navbar-fixed');    
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+
+}
+
+// hamburger
+const humberger = document.querySelector('#humberger');
+const navMenu = document.querySelector('#nav-menu')
+
+humberger.addEventListener('click', function(){
+  humberger.classList.toggle('humberger-active');
+  navMenu.classList.toggle('hidden');
+});
